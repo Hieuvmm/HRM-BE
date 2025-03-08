e7087eb5aa17d1353a982fc5eb47d2d36537f84b
TueTN <tangoctue1502@gmail.com>
2025-03-08 12:40:33 +0700

    update supper function for child class

--- admin-service/src/main/java/com/vworks/wms/admin_service/model/requestBody/PostSearchBranchRequestBody.java
+++ admin-service/src/main/java/com/vworks/wms/admin_service/model/requestBody/PostSearchBranchRequestBody.java
@@ -2,8 +2,10 @@ package com.vworks.wms.admin_service.model.requestBody;
 
 import com.vworks.wms.common_lib.base.PaginationRequest;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class PostSearchBranchRequestBody extends PaginationRequest {
     private String status;
 }
